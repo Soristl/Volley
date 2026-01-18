a43efc78e07cd9465ec20918f5e6e4abe202878b
Soristl <soris.com1@gmail.com>
2026-01-18 14:03:16 -0300

    feat: add function to set author map name and reformuled the admins command

---
+++ src/events/eventNewGame.lua
@@ -0,0 +1,53 @@
+function eventNewGame()
+  if mode == "gameStart" then
+    if gameStats.teamsMode or gameStats.twoTeamsMode then
+      if gameStats.isCustomMap then
+        ui.setMapName("<j>"..customMapsFourTeamsMode[gameStats.customMapIndex][4].."<n>")
+
+        return
+      end
+
+      if gameStats.totalVotes >= 2 then
+        ui.setMapName("<j>"..customMapsFourTeamsMode[gameStats.mapIndexSelected][4].."<n>")
+
+        return
+      end
+
+      ui.setMapName("<j>Refletz#6472<n>")
+
+      return
+    end
+
+    if gameStats.threeTeamsMode then
+      if gameStats.isCustomMap then
+        ui.setMapName("<j>"..customMapsThreeTeamsMode[gameStats.customMapIndex][4].."<n>")
+
+        return
+      end
+
+      if gameStats.totalVotes >= 2 then
+        ui.setMapName("<j>"..customMapsThreeTeamsMode[gameStats.mapIndexSelected][4].."<n>")
+
+        return
+      end
+
+      ui.setMapName("<j>Refletz#6472<n>")
+
+      return
+    end
+
+    if gameStats.isCustomMap then
+      ui.setMapName("<j>"..customMaps[gameStats.customMapIndex][4].."<n>")
+
+      return
+    end
+
+    if gameStats.totalVotes >= 2 then
+      ui.setMapName("<j>"..customMaps[gameStats.mapIndexSelected][4].."<n>")
+
+      return
+    end
+
+    ui.setMapName("<j>Refletz#6472<n>")
+  end
+end
